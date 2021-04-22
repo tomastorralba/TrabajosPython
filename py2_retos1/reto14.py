9d46d685bc882d7b3777f9e90515b03957043440
TOMAS SANTIAGO SANTIAGO <>
2021-04-22 20:36:25 +0000

    reto 8 y 14

---
+++ py2_retos1/reto14.py
@@ -0,0 +1,8 @@
+def numero_al_2(x):
+  return x**2
+for i in range(-10,11):
+  print(numero_al_2(i))
+def fin():
+  print('Programa finalizado')
+
+fin()
---
+++ py2_retos1/reto8.py
@@ -0,0 +1,5 @@
+print("los numeros pares entre 10 y 20 son")
+for i in range (9,21):
+  c=i % 2 
+  if c == 0:
+    print(i)
