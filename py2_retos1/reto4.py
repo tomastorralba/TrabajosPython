ea14927c64cb1f202f3b67eaca215420619abf2a
TOMAS SANTIAGO SANTIAGO <>
2021-04-22 19:53:01 +0000

    reto 4 y 5

---
+++ py2_retos1/reto4.py
@@ -0,0 +1,9 @@
+numero_1=input("ingrese el primer numero : ")
+numero_2=input("ingrese el segundo numero : ")
+if numero_1 <numero_2:
+    print("el mayor nuemro fue el segundo :"+ numero_2)
+
+if numero_1 >numero_2:
+    print("el mayor nuemro fue el primero :"+ numero_1)
+if numero_1 ==numero_2:
+    print("los numeros son iguales ")
