171351112813b03ea42ba9d003acebfa3a99886e
TOMAS SANTIAGO SANTIAGO <81332942+tomastorralba@users.noreply.github.com>
2021-08-11 23:48:40 +0000

    problema

--- py5_matplotlib/plot_list.py
+++ py5_matplotlib/plot_list.py
@@ -1,3 +1,29 @@
 import matplotlib.pyplot as plot
+b=1
+i=1
+lista =[]
+numero = int(input('ingrese el numero a evaluar :'))
+c = numero % 2 
+if c == 0:
+  lista.append(numero)
+  b=2
+if b ==1:
+  lista.append(numero)
+  numero= numero *3 +1
+  lista.append(numero)
+while i >0:
+  numero=numero / 2
+  lista.append(numero)
+  if numero == 1:
+    i=0
+  else:
+    i=1
 
-numero = float(input('ingrese el numero a evaluar')or 60)
+
+
+
+print("la lista de numero es :")
+print(lista)
+
+
+
