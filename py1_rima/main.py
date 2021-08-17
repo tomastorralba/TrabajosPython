0394c12e82e4ac401c5e65e3ad442a6cc1fe560a
TOMAS SANTIAGO SANTIAGO <81332942+tomastorralba@users.noreply.github.com>
2021-08-17 12:21:39 +0000

    ejercicio finalizado

--- py1_rima/main.py
+++ py1_rima/main.py
@@ -1,4 +1,5 @@
-print('hola mundo')
+a=2
+print('hola mundo',a)
 
 archivo = open ('palabras500.csv', encoding="utf-8")
 lineas = archivo.readlines()
--- py5_matplotlib/plot_list.py
+++ py5_matplotlib/plot_list.py
@@ -1,29 +1,15 @@
 import matplotlib.pyplot as plot
-b=1
-i=1
 lista =[]
 numero = int(input('ingrese el numero a evaluar :'))
-c = numero % 2 
-if c == 0:
-  lista.append(numero)
-  b=2
-if b ==1:
-  lista.append(numero)
-  numero= numero *3 +1
-  lista.append(numero)
-while i >0:
-  numero=numero / 2
-  lista.append(numero)
-  if numero == 1:
-    i=0
+lista.append(numero)
+while numero != 1:
+  
+  if (numero % 2)== 0:
+    numero=numero // 2
   else:
-    i=1
+    numero = numero * 3 + 1 
+  lista.append(numero)     
+print("la lista de numero es :" , lista)
 
 
-
-
-print("la lista de numero es :")
-print(lista)
-
-
-
+ 
